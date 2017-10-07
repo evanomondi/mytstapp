5d08e2d7b51cf69c025f7e0b313b94ff77368d11
Evan <evanomondi7@gmail.com>
2017-10-07 14:43:43 +0300

    new updates

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-mauron85-background-geolocation/www/backgroundGeolocation.js",
+        "id": "cordova-plugin-mauron85-background-geolocation.backgroundGeolocation",
+        "pluginId": "cordova-plugin-mauron85-background-geolocation",
+        "clobbers": [
+            "backgroundGeolocation"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-mauron85-background-geolocation": "2.2.5"
+}
+// BOTTOM OF METADATA
+});
---
+++ platforms/browser/www/js/config.js
@@ -0,0 +1,7 @@
+/*Kartero App Configuration*/
+var krms_driver_config ={		
+	'ApiUrl':"http://laurelsmacrotech.com/delivery/api",					
+	'DialogDefaultTitle':"Laurels Delivery App",	
+	'PushProjectID':"",	
+	'APIHasKey':"625439113984"
+};
